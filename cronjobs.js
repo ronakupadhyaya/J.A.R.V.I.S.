d9ea8d7586b6ca7611c6a7b1e4b9daf6279084f0
David Huang <davidhuang972@gmail.com>
2017-07-19 12:33:55 -0700

    close cronjob

--- cronjobs.js
+++ cronjobs.js
@@ -3,13 +3,17 @@ import axios from 'axios';
 import express from 'express';
 import { messageConfirmation, getQueryParams } from './constants';
 import { User, Reminder } from './models';
+import moment from 'moment';
 
 const router = express.Router();
 const botToken = process.env.SLACK_BOT_TOKEN || '';
 const rtm = new RtmClient(botToken);
 const channel = 'T6AVBE3GX';
 
-const currentDate = new Date().toISOString().substring(0, 10);
+const currentDate = new Date();
+const yesterday = new Date(currentDate.getTime());
+yesterday.setDate(yesterday.getDate() - 1);
+
 
 rtm.start();
 
@@ -19,7 +23,11 @@ rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
       for(var i = 0; i < reminders.length; i++) {
         var reminder = reminders[i];
         console.log(reminder.date, currentDate);
-        if(reminder.date === currentDate) {
+        if(reminder.date === currentDate.toISOString().slice(0, 10)) {
+          console.log("Today");
+          rtm.sendMessage(reminder.subject, reminder.userId);
+        } else if(reminder.date === yesterday.toISOString().slice(0, 10)) {
+          console.log("Yesterday");
           rtm.sendMessage(reminder.subject, reminder.userId);
         }
       }
