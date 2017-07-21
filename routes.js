071ce96525904f1a3ae8f7e05008ece0923e92e3
jackkimball13 <john.m.kimball@uconn.edu>
2017-07-21 12:23:44 -0700

    conflict

--- routes.js
+++ routes.js
@@ -110,7 +110,8 @@ router.post('/slack/interactive', (req, res) => {
                 getFreeBusy(userAuth, "2017-07-20T23:44:28.917Z", "2017-07-21T23:44:28.917Z", userEmail)
                   .then((response) => {
                     console.log( "Keys of response are", Object.keys(response));
-                    console.log( "Response is", response.calendars, "Type of response is", typeof response.calendars);
+                    console.log("Test response is", response.calendars['naperry@wisc.edu'].busy);
+                    console.log("Keys of response are", Object.keys(response.calendars));
                     console.log("Out of for-loop");
                   })
                   .catch((err2) => {
