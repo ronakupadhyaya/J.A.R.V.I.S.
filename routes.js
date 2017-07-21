8dec1d266150abc2747e629cbe89c5ce3466680c
jackkimball13 <john.m.kimball@uconn.edu>
2017-07-21 12:15:05 -0700

    conflict

--- routes.js
+++ routes.js
@@ -109,7 +109,7 @@ router.post('/slack/interactive', (req, res) => {
                 userAuth.setCredentials(pendingUser.google);
                 getFreeBusy(userAuth, "2017-07-20T23:44:28.917Z", "2017-07-21T23:44:28.917Z", userEmail)
                   .then((response) => {
-                    console.log( "Response is", response.userEmail.busy);
+                    console.log( "Response is", response.userEmail);
                     console.log("Out of for-loop");
                   })
                   .catch((err2) => {
